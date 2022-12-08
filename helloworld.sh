b05d1fec02e6181aac2da0e42e36934090325c5d
Ashley <arbryce@yahoo.com>
2022-12-07 19:02:51 -0600

    update

---
+++ helloworld.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Ashley Bryce
+# Date of latest revision: 12/7/22
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo I am working perfectly
+# End
