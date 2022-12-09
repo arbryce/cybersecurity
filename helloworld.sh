b903686417568b8d55832528fd0929462f9d14af
arbryce <118229904+arbryce@users.noreply.github.com>
2022-12-08 18:49:04 -0600

    Update helloworld.sh

--- helloworld.sh
+++ helloworld.sh
@@ -2,7 +2,7 @@
 
 # Script: Ops 201 Class 02 Ops Challenge Solution
 # Author: Ashley Bryce
-# Date of latest revision: 12/7/22
+# Date of latest revision: 12/8/22
 # Purpose: Print a string to the terminal
 
 
