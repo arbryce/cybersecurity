918709ef9a0edc01cc045878d762a52f9ba8416d
Ashley <arbryce@yahoo.com>
2022-12-12 20:24:44 -0600

    update

--- helloworld.sh
+++ helloworld.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-# Script: Ops 201 Class 02 Ops Challenge Solution
+# Script: Ops 201 Class 04 Ops Challenge Solution
 # Author: Ashley Bryce
-# Date of latest revision: 12/7/22
+# Date of latest revision: 12/12/22
 # Purpose: Print a string to the terminal
 
 
 # Main
-$a last
-function users(){
-    
-    echo $a
-};
+array=("dir1" "dir2" "dir3" "dir4")
+mkdir ${array[*]} 
+touch ${array[0]}/file.txt
+touch ${array[1]}/file.txt
+touch ${array[2]}/file.txt
+touch ${array[3]}/file.txt
 #End
