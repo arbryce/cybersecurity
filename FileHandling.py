4c43fbe5d4133e3c83804751c036412ea9391345
Ashley <arbryce@yahoo.com>
2023-02-16 19:12:10 -0600

    301 OC 10

---
+++ FileHandling.py
@@ -0,0 +1,20 @@
+# 301 Ops Challenge 10
+# Using file handling commands, create a Python script that creates a new .txt file, appends three lines, 
+# prints to the screen the first line, then deletes the .txt file.
+
+# Main
+
+f = open("new.txt", "w")
+l1 = "Hello \n" 
+l2 = "Howdy \n" 
+l3 = "Hi \n"
+
+f.writelines([l1, l2, l3])
+
+f = open("new.txt", "r")
+print(l1)
+
+import os
+os.remove("new.txt")
+
+# End
