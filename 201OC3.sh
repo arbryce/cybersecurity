9936a5a5558102dd1e823f2f800529134121fb72
Ashley <arbryce@yahoo.com>
2023-04-26 20:26:40 -0500

    201OC3

---
+++ 201OC3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 03 Ops Challenge Solution
+# Author: Ashley Bryce
+# Date of latest revision: 4/26/23
+# Purpose: Print a string to the terminal
+
+# Main
+
+sudo last /var/log/wtmp
+sudo last -3
+
+# End
