a2f57a8dbfb9150cde57bb933bee7eae297124a1
arbryce <arbryce@yahoo.com>
2023-05-08 18:58:10 -0500

    201 Ops Chal 11

---
+++ Cyber TA/201OC11.ps1
@@ -0,0 +1,21 @@
+# Script: Ops 201 Class 11 Ops Challenge Solution
+# Author: Ashley Bryce
+# Date of latest revision: 05/11/23
+# Purpose: Print a string to the terminal
+
+# Main
+
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
+
+netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
+
+Enable-NetFirewallRule -DisplayGroup “Remote Desktop”
+
+iex ((New-Object 
+System.Net.WebClient).DownloadString('https://git.io/debloat'))
+
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
+
+Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
+
+# End
