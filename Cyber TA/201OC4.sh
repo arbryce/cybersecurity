55b80ff10bf0e0b02d5b94913a358ecf9d0c5bd5
Ashley <arbryce@yahoo.com>
2023-05-08 18:28:46 -0500

    201 Ops Chal 4

--- Cyber TA/201OC4.sh
+++ Cyber TA/201OC4.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 04 Ops Challenge Solution
+# Author: Ashley Bryce
+# Date of latest revision: 
+# Purpose: Print a string to the terminal
+
+
+# Main
+
 mkdir dir1
 mkdir dir2
 mkdir dir3
@@ -9,3 +19,5 @@ touch ${folders[0]}/text.txt
 touch ${folders[1]}/text.txt
 touch ${folders[2]}/text.txt
 touch ${folders[3]}/text.txt
+
+# End
