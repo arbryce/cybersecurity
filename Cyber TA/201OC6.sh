66149250443be37be55e06a934a88b8d33103cfc
Ashley <arbryce@yahoo.com>
2023-05-08 18:24:07 -0500

    201 Ops Chal 6

---
+++ Cyber TA/201OC6.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 06 Ops Challenge Solution
+# Author: Ashley Bryce
+# Date of latest revision: 12/15/22
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+if cat "hey.txt" 
+then echo "Already here..."
+
+else
+    touch "hey.txt"
+    echo "I'm alive!"
+    fi
+
+# End
