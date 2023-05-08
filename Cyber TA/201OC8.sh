a861779eff6f9a0d1ee803f0c7536ade51ae3085
Ashley <arbryce@yahoo.com>
2023-05-08 18:17:59 -0500

    201 Ops Chal 8

---
+++ Cyber TA/201OC8.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 09 Ops Challenge Solution
+# Author: Ashley Bryce
+# Date of latest revision: 12/22/22
+# Purpose: Print a string to the terminal
+
+
+# Main
+
+@echo off
+ROBOCOPY "C:\Users\j.thompson\Desktop" "E:" /E
+pause
+
+# End
