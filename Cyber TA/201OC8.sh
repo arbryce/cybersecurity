eb86e4ca9628222192fffebfab20ddc29c614f0d
arbryce <arbryce@yahoo.com>
2023-05-08 19:02:00 -0500

    201 Ops Chal 8

--- Cyber TA/201OC8.sh
+++ Cyber TA/201OC8.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Script: Ops 201 Class 09 Ops Challenge Solution
+# Script: Ops 201 Class 08 Ops Challenge Solution
 # Author: Ashley Bryce
 # Date of latest revision: 12/22/22
 # Purpose: Print a string to the terminal
