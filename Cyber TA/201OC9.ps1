e9c877871a8e0b047ba2143dc8717534acb010bb
arbryce <arbryce@yahoo.com>
2023-05-08 19:00:04 -0500

    201 Ops Chal 9

---
+++ Cyber TA/201OC9.ps1
@@ -0,0 +1,12 @@
+# Script: Ops 201 Class 09 Ops Challenge Solution
+# Author: Ashley Bryce
+# Date of latest revision: 05/09/23
+# Purpose: Print a string to the terminal
+
+# Main
+
+$Begin = Get-Date -Date '12/21/2022 00:00:00'
+$End = Get-Date -Date '12/22/2022 23:59:59'
+Get-EventLog -LogName System -EntryType Error -After $Begin -Before $End
+
+# End
