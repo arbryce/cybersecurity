e99d2ed58f8536a84cfcaf2050a0024f7e7fd8f4
arbryce <arbryce@yahoo.com>
2023-05-09 19:32:04 -0500

    201 Ops Chal 9

--- Cyber TA/201OC9.ps1
+++ Cyber TA/201OC9.ps1
@@ -6,7 +6,7 @@
 # Task 1
 $Begin = Get-Date -Date '12/21/2022 00:00:00'
 $End = Get-Date -Date '12/22/2022 23:59:59'
-Get-EventLog -LogName System -EntryType Error -After $Begin -Before $End > C:\Users\TJVog\Documents\last_24.txt
+Get-EventLog -LogName System -After $Begin -Before $End > C:\Users\TJVog\Documents\last_24.txt
 
 # Task 2
 Get-EventLog -LogName System -EntryType Error > C:\Users\TJVog\Documents\error.txt
