cdf67e93379e92fd768a44af69d02e128f9b0452
arbryce <arbryce@yahoo.com>
2023-05-15 15:25:25 -0500

    201 Ops Chal 11

--- Cyber TA/201OC11.ps1
+++ Cyber TA/201OC11.ps1
@@ -5,17 +5,22 @@
 
 # Main
 
+# Enable File and Printer Sharing
 Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled True
 
+# Allow ICMP traffic
 netsh advfirewall firewall add rule name="Allow incoming ping requests IPv4" dir=in action=allow protocol=icmpv4
 
+#Enable Remote management
 Enable-NetFirewallRule -DisplayGroup “Remote Desktop”
 
-iex ((New-Object 
-System.Net.WebClient).DownloadString('https://git.io/debloat'))
+# Remove bloatware
+iex ((New-Object System.Net.WebClient).DownloadString('https://git.io/debloat'))
 
+# Enable Hyper-V
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
 
+# Disable SMBv1, an insecure protocol
 Set-SmbServerConfiguration -EnableSMB1Protocol $false -Force
 
 # End
