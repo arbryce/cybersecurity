cfaa64280bc25be71b02a4d0e64ec9e9135e32be
arbryce <arbryce@yahoo.com>
2023-05-15 15:38:45 -0500

    201 Ops Chal 9

--- Cyber TA/201OC9.ps1
+++ Cyber TA/201OC9.ps1
@@ -3,20 +3,20 @@
 # Date of latest revision: 12/22/23
 # Purpose: Print a string to the terminal
 
-# Task 1
+# Task 1: Output all events from the System event log that occurred in the last 24 hours to a file on your desktop named last_24.txt
 $Begin = Get-Date -Date '12/21/2022 00:00:00'
 $End = Get-Date -Date '12/22/2022 23:59:59'
 Get-EventLog -LogName System -After $Begin -Before $End > C:\Users\TJVog\Documents\last_24.txt
 
-# Task 2
+# Task 2: Output all “error” type events from the System event log to a file on your desktop named errors.txt
 Get-EventLog -LogName System -EntryType Error > C:\Users\TJVog\Documents\error.txt
 
-# Task 3
+# Task 3: Print to the screen all events with ID of 16 from the System event log
 Get-EventLog -LogName System -InstanceID 16 > C:\Users\TJVog\Documents\InstanceID.txt
 
-# Task 4
+# Task 4: Print to the screen the most recent 20 entries from the System event log
 Get-EventLog -LogName System -Newest 20 > C:\Users\TJVog\Documents\New20.txt
 
-# Task 5
+# Task 5: Print to the screen all sources of the 500 most recent entries in the System event log. Ensure that the full lines are displayed (get rid of the … and show the entire text)
 $Events = Get-EventLog -LogName System -Newest 500
 $Events | Group-Object -Property Source -NoElement | Sort-Object -Property Count -Descending > C:\Users\TJVog\Documents\500.txt
