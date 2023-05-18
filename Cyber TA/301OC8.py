197fa4f60118e98fd14856932ea59e2e74ae6f41
arbryce <arbryce@yahoo.com>
2023-05-17 19:07:06 -0500

    301 Ops Chal 8

---
+++ Cyber TA/301OC8.py
@@ -0,0 +1,12 @@
+# Assign to a variable a list of ten string elements.
+my_list = ["apple", "banana", "cherry", "strawberry", "blueberry", "fig", "grape", "orange", "lemon", "avocado"]
+
+# Print the fourth element
+print(my_list[3])
+
+# Print the sixth through tenth elements
+print(my_list[5:])
+
+# Change the value of the seventh element
+my_list[6] = "onion"
+print(my_list)
