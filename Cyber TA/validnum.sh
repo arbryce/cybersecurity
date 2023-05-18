4ae10a62da0b8f31f9bfe4108952a0579c5f3f3a
arbryce <arbryce@yahoo.com>
2023-05-17 19:25:18 -0500

    Additional Challenge

---
+++ Cyber TA/validnum.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# Bob is back at it again! Now he needs us to script something that if user inserts between two to 5, it will print out “Valid Number” and “your number is ___”.
+# And if the user input is not between 2 and 5, it will print out “not valid!”
+# Think about it and we will work on this together in class today! (edited)
+#Main
+# -eq = equal
+# -ne = are not equal
+# -gt = greater than
+# -ge = greater or equal to
+# -lt = less than
+# -le = less than or equal to
+# >= (Greater than or equal to)
+# <= (Less than or equalk to)
+# > (Greater)
+# < (Less)
+# == (comparison)
+# % (Remainder)
+# * (Multiply)
+
+echo "Enter a number between 1 and 10."
+
+read number
+
+if (($number >= 2 && $number <= 5 ))
+    then echo "Valid number."
+
+else
+    echo "Invalid number - try again."
+
+fi        
+
+echo "Your number is $number."
