1a4e453a5da1870b2c790e0df3295c07c8919d31
arbryce <arbryce@yahoo.com>
2023-05-17 19:22:23 -0500

    Additional Challenge

---
+++ Cyber TA/countms.py
@@ -0,0 +1,14 @@
+# Your task is very simple here: write a program that uses a for loop to "count mississippi" to five.
+# Having counted to five, the program should print to the screen the final message "Ready or not, here I come!"
+
+# Main
+
+import time
+
+for i in range (1, 6):
+    time.sleep(1)
+    print(i, 'Mississipi')
+
+print ("Ready or not, here I come!")
+
+# End
