b5e00d3077eb32011251d735dd383e697cf962c0
arbryce <arbryce@yahoo.com>
2023-05-17 19:08:03 -0500

    301 Ops Chal 9

---
+++ Cyber TA/301OC9.py
@@ -0,0 +1,24 @@
+# Create if statements using these logical conditionals below. Each statement should print information to the screen depending on if the condition is met.
+# Equals: a == b
+# Not Equals: a != b
+# Less than: a < b
+# Greater than: a > b
+# Create an if statement using a logical conditional of your choice and include elif keyword that executes when other conditions are not met.
+# Create an if statement that includes both elif and else to execute when both if and elif are not met.
+
+# Main
+
+a = input("Please enter a number for a: ")
+b = input("Please enter a number for b: ")
+
+if (a == b) :
+    print("A and b are equal.")
+
+elif (a != b) :
+    print("Does not equal.")
+    if (a < b) :
+        print("A is less than.")
+    elif (a > b) :
+        print("A is greater than.")
+
+# End
