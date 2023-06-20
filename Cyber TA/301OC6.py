891832e52f71c964416058e92e25d190808f41cf
arbryce <arbryce@yahoo.com>
2023-06-19 19:29:55 -0500

    301 Ops Chal 6

--- Cyber TA/301OC6.py
+++ Cyber TA/301OC6.py
@@ -3,7 +3,7 @@
 # Date of latest revision: 02/09/23
 # Create a Python script that executes a few bash commands successfully (currently on windows machine, script is written for windows)
 
-# Main
+# Powershell/Python:
 
 import os
 
@@ -15,4 +15,21 @@ print(os.system(define_user))
 print(os.system(define_ip))
 print(os.system(define_hardware))
 
-# End
+
+
+# Bash/Python script:
+
+import os
+
+whoami = os.popen('whoami').read()
+ip = os.popen('ip a').read()
+lshw = os.popen('lshw -short').read()
+
+print("Output of 'whoami' command:")
+print(whoami)
+
+print("Output of 'ip a' command:")
+print(ip)
+
+print("Output of 'lshw -short' command:")
+print(lshw)
